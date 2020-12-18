a06f5d63f0bd18f91fc2c965ad10c515d8cfcee3
Carlos Peralta <carlos9917@gmail.com>
2020-12-18 13:10:13 +0100

    Deleted some old files and changed some paths in deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,55 +1,49 @@
 #!/bin/bash
+#Deploy the container, either production or development
+
 source env.sh
 IMAGENAME=grassdev
 CONTAINERNAME=grassrun # Name of the container based on IMAGENAME
+build=1 #default: only build
 
-#copy template and ask user to replace password
-if [ -z $1 ]; then
-  echo Please provide password for github repo
+if [ -z $1 ] && [ -z $2 ]; then
+  echo "Please provide which template to use (dev or prod) and build/run "
+  echo "Example: dev 1 (1 for build only, 2 for build and run)"
   exit
 else
-  gitpas=$1
+  dfile=$1 #dev or prod
+  build=$2 #1 for build only, 2 for build and run
 fi
 
-#Decide which template to use
-if [ -z $2 ]; then
-  dfile="dev"
-else
-  dfile=$2
-fi
 echo Using Dockerfile.temp.$dfile template
-
+[ ! -f ./Dockerfile.temp.$dfile ] && echo "ERROR: $dtype unknown"
 cp Dockerfile.temp.$dfile Dockerfile
-cp ../../test_scripts/crontab .
-cp ../../test_scripts/run-crond.sh .
-chmod -v +x ./run-crond.sh
 
 #Build the Image
-#docker build -t $IMAGENAME .
 docker build --build-arg PID=$PID --build-arg GID=$GID -t $IMAGENAME .
-#docker build --user "$(id -u):$(id -g)" --build-arg PID=$PID --build-arg GID=$GID -t $IMAGENAME .
 
-#Stop any previous containers with this name before running
-if [ ! "$(docker ps -q -f name=$CONTAINERNAME)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=$CONTAINERNAME)" ]; then
-        # cleanup
-        echo "Removing container $CONTAINERNAME"
-        docker rm $CONTAINERNAME
-    fi
+if [ $build == 2 ]; then
+  #Stop any previous containers with this name before running
+  if [ ! "$(docker ps -q -f name=$CONTAINERNAME)" ]; then
+      if [ "$(docker ps -aq -f status=exited -f name=$CONTAINERNAME)" ]; then
+          # cleanup
+          echo "Removing container $CONTAINERNAME"
+          docker rm $CONTAINERNAME
+      fi
+  else
+      docker stop $CONTAINERNAME
+      docker rm $CONTAINERNAME
+  fi
+  
+  
+  # create directory container-data if not present. If I don't create before,
+  # it will be owned by root!
+  #Run the image in container
+  [ -d $WRKDIR/container-data ] || mkdir -p $WRKDIR/container-data
+  docker run -dit -P --name $CONTAINERNAME -v $WRKDIR/container-data:/data $IMAGENAME
+  
+  #Send STDOUT from container
+  docker logs -f $CONTAINERNAME > docker.log
 else
-    docker stop $CONTAINERNAME
-    docker rm $CONTAINERNAME
+  echo Only building container. Select 2 for running
 fi
-
-
-# create directory container-data if not present. If I don't create before,
-# it will be owned by root!
-#Run the image in container
-[ -d $WRKDIR/container-data ] || mkdir -p $WRKDIR/container-data
-docker run -dit -P --name $CONTAINERNAME -v $WRKDIR/container-data:/data $IMAGENAME
-
-#Send STDOUT from container
-docker logs -f $CONTAINERNAME > docker.log
-cp Dockerfile.temp.$dfile Dockerfile #so it doesnt save the one with the password
-rm -f ./crontab
-rm -f ./run-crond.sh
--- docker/run-crond.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#crond -L /var/log/cron/cron.log "$@" && tail -f /var/log/cron/cron.log
-cron -L /var/log/cron/cron.log "$@" && tail -f /var/log/cron/cron.log
